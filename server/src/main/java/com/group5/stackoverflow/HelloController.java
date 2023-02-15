59cebff772c1422832245c89a3c3635c22719c1f
gnswp21 <gnswp21@gmail.com>
2023-02-15 17:59:45 +0900

    init project

---
+++ server/src/main/java/com/group5/stackoverflow/HelloController.java
@@ -0,0 +1,14 @@
+package com.group5.stackoverflow;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping
+    public String hello(){
+        return "hello, group5!";
+    }
+}
